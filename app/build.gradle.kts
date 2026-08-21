64849f92f334c09041f9c458e085354de9b6e934
chelovek6480-hub <chelovek6480@gmail.com>
2026-08-21 21:07:26 +0400

    Add appcompat, fragment, recyclerview dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,4 +26,8 @@ android {
 
 dependencies {
     implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
